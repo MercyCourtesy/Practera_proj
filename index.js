6ddd7682e252de22802f0f65325db869bd6317fc
Rahma <rahma.awais.khalif@gmail.com>
2021-03-20 14:52:18 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+      /*Cards*/
+      $('.card').click(function(){
+          $(this).toggleClass('flipped');
+        });
+
+//Nav Bar
+$(function() {
+  $(".toggle").on("click", function() {
+      if ($(".item").hasClass("active")) {
+          $(".item").removeClass("active");
+          $(this).find("a").html("<i class='fas fa-bars'></i>");
+      } else {
+          $(".item").addClass("active");
+          $(this).find("a").html("<i class='fas fa-times'></i>");
+      }
+  });
+});
+
